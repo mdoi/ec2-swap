8a52381e0b5a9d02ca6ef29cabaf9c9425203cf1
Ryuta Otaki <otaki.ryuta@classmethod.jp>
2014-02-24 09:11:25 +0000

    initial commit

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+fpm -s dir -t rpm \
+	-v 0.1.0 \
+	-n ec2-swap \
+	-a noarch \
+	-C ./source \
+	-p ./repo/yum/x86_64 \
+	--maintainer "Ryuta Otaki<otaki.ryuta@classmethod.jp>" \
+	--vendor "Classmethod, Inc." \
+	--prefix / \
+	--license "ASL 2.0" \
+	--url "http://dev.classmethod.jp/" \
+	--description "ec2-swap is to create swapon swap image on EC2 instance store" \
+	-d util-linux-ng \
+	--after-install ./script/postinstall \
+	--before-remove ./script/preuninstall \
+	.
+
+cd ./repo/yum/x86_64
+createrepo -v .
